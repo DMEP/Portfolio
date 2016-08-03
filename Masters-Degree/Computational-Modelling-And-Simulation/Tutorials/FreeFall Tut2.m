ce0ebf24c883bb18bfca3a361fb5c32855507871
DMEP <DMElstob@gmail.com>
2016-08-03 01:02:14 +0100

    Add files via upload

---
+++ Masters-Degree/Computational-Modelling-And-Simulation/Tutorials/FreeFall Tut2.m
@@ -0,0 +1,3 @@
+g = 9.8; % Gravitational acceleration constant (m/s^2)
+t = 0:10; % Vector of Time
+d = 0.5.*g*t.^2; % Equation of free falling body
---
+++ Masters-Degree/Computational-Modelling-And-Simulation/Tutorials/NonVect Tut4.m
@@ -0,0 +1,6 @@
+NonVect = zeros(1, 1000); 
+for i = 1 : 1000
+NonVect(i) = 2 * i
+end;
+
+
